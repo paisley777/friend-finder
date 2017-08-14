9aff10c95e47f9c994971759bafcd14a900ac459
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-08-13 20:23:25 -0500

    set up html routes

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+//Requiring the path package
+var path = require("path");
+
+//Setting up routes using HTML GET requests
+module.exports = function(app) {
+  app.get("/home", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // If no matching route is found default to home
+  app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
