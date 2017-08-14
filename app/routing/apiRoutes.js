c2378ce0c95147e97246de7165dc8181ef9765f4
paisley777 <jthurk@northwestern.edu git config --global user.name Your>
2017-08-13 20:55:11 -0500

    set up html pages

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,16 @@
+//Require the file containing the data that the routes will be linked to
+var friendsData = require("../data/friendsData");
+
+//Setting up routing
+module.exports = function(app) {
+  //Get and display the data in the friendsData file
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  //Post a new friend object to the array in the friendsData.js file 
+  app.post("/api/friends", function(req, res) {
+    friendsData.push(req.body);
+    //res.json(true);
+  });
+};
